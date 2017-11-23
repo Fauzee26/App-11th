e64a94e72fccd32890de442f6ae9077431bd2ea0
Fauzee26 <32213481+Fauzee26@users.noreply.github.com>
2017-11-23 08:52:27 +0700

    juz 27

---
+++ Juz 27/Juz 27/FirstViewController.swift
@@ -0,0 +1,90 @@
+//
+//  FirstViewController.swift
+//  Juz 27
+//
+//  Created by Muhammad Hilmy Fauzi on 23/11/17.
+//  Copyright © 2017 Hilmy Corp. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+var surahs:[String] = []
+var audioPlayer = AVAudioPlayer()
+var thisSurah = 0
+var audioStuffed = false
+
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var myTableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return surahs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = surahs[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        do{
+            let audioPath = Bundle.main.path(forResource: surahs[indexPath.row], ofType: ".mp3")
+            try audioPlayer = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!) as URL)
+            audioPlayer.play()
+            thisSurah = indexPath.row
+            audioStuffed = true
+        }
+        catch
+        {
+            print("ERROR")
+        }
+    }
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        getSurahName()
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func getSurahName()
+    {
+        let folderURL = URL(fileURLWithPath: Bundle.main.resourcePath!)
+        
+        do
+        {
+            let surahPath = try FileManager.default.contentsOfDirectory(at: folderURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+            
+            for surah in surahPath
+            {
+                var mySurah = surah.absoluteString
+                
+                if mySurah.contains(".mp3")
+                {
+                    let findString = mySurah.components(separatedBy: "/")
+                    mySurah = findString[findString.count-1]
+                    mySurah = mySurah.replacingOccurrences(of: "%20", with: " ")
+                    mySurah = mySurah.replacingOccurrences(of: ".mp3", with: "")
+                    surahs.append(mySurah)
+                }
+            }
+            
+            myTableView.reloadData()
+        }
+        catch
+        {
+            
+        }
+    }
+
+}
+
---
+++ Juz 27/Juz 27/SecondViewController.swift
@@ -0,0 +1,80 @@
+//
+//  SecondViewController.swift
+//  Juz 27
+//
+//  Created by Muhammad Hilmy Fauzi on 23/11/17.
+//  Copyright © 2017 Hilmy Corp. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBAction func play(_ sender: Any) {
+        if audioStuffed == true && audioPlayer.isPlaying == false
+        {
+            audioPlayer.play()
+        }
+    }
+    @IBAction func pause(_ sender: Any) {
+        if audioStuffed == true && audioPlayer.isPlaying
+        {
+            audioPlayer.pause()
+        }
+        
+    }
+    @IBAction func prev(_ sender: Any) {
+        if thisSurah != 0 && audioStuffed == true
+        {
+            playThis(thisOne: surahs[thisSurah-1])
+            thisSurah -= 1
+            label.text = surahs[thisSurah]
+        }else{
+            
+        }
+    }
+    @IBAction func next(_ sender: Any) {
+        if thisSurah < surahs.count-1 && audioStuffed == true
+        {
+            playThis(thisOne: surahs[thisSurah+1])
+            thisSurah += 1
+            label.text = surahs[thisSurah]
+
+        }else{
+            
+        }
+    }
+    @IBAction func slider(_ sender: UISlider) {
+        if audioStuffed == true{
+            audioPlayer.volume = sender.value
+        }
+    }
+    
+    func playThis(thisOne:String)
+    {
+        do{
+            let audioPath = Bundle.main.path(forResource: thisOne, ofType: ".mp3")
+            try audioPlayer = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!) as URL)
+            audioPlayer.play()
+        }
+        catch
+        {
+            print("ERROR")
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text = surahs[thisSurah]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
